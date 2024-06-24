089a6d3058e888f4e012ce1f6d29c5bd93e83d97
cothao <114521616+cothao@users.noreply.github.com>
2024-06-24 16:04:29 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const config = {
+    type: Phaser.AUTO,
+    parent: "phaser-example",
+    width: 800,
+    height: 600,
+    physics: {
+      default: "arcade",
+      arcade: {
+        gravity: { y: 300 },
+        debug: false,
+      },
+      pixelArt: true,
+      roundPixels: false,
+    },
+    scene: {
+      preload: preload,
+      create: create,
+      update: update,
+    },
+  };
+  
+  function preload() {
+
+
+  }
+  
+  
+  function create() {
+  
+  
+  }
+  
+  
+  function update() {
+  
+  
+  }
+  
+  const game = new Phaser.Game(config);
